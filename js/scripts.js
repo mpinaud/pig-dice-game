5f0c0fbf5a06f744c41decb03657d398c040d865
Chris Johnson and Mikey Pinaud <chris@>
2017-11-01 11:49:43 -0700

    add object to store player info

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,28 @@ function Player(playerName){
   this.score = 0;
 }
 
-function endTurn(players){
-  if (endTurn = 0){
-    endTurn = 1;
+function endTurn(turn){
+  if (turn = 0){
+    turn = 1;
   } else {
-    endTurn = 0;
+    turn = 0;
   }
+  return turn;
 }
 
-function playGame(players){
+function playGame(players, clickedRollOrHold){
+  var currentPlayer = players.turn;
+  if (clickedRollOrHold === 'roll') {
+    var numberRolled = rollDice();
+    if (numberRolled > 1) {
+      players[currentPlayer].score += numberRolled;
+    } else {
+      tempScore = 0;
+      endTurn(turn);
+    }
+  } else {
+
+  }
   var keepPlaying = true;
   while (keepPlaying){
     players.forEach(function(player){
@@ -32,22 +45,28 @@ function playGame(players){
   }
 }
 
-
 $(function(){
   var players;
+  var clickedRollOrHold;
   $("button#name-button").click(function(){
-    players = [];
+    players = {};
     var player1Name = $("#player-1-name-input").val();
     var player2Name = $("#player-2-name-input").val();
-    players.push(new Player(player1Name));
-    players.push(new Player(player2Name));
+    players.player1 = new Player(player1Name);
+    players.player2 = new Player(player2Name);
+    players.turn = 'player1';
     console.log(players);
   });
   $("button#start-game-button").click(function(){
     // $("").hide() // hide upper area
-    playGame(players);
+    // show lower game area
   });
-
-  $("")
-
+  $("button#roll-button").click(function(){
+    clickedRollOrHold = 'roll';
+    playGame(players, clickedRollOrHold);
+  });
+  $("button#hold-button").click(function(){
+    clickedRollOrHold = 'hold';
+    playGame(players, clickedRollOrHold);
+  });
 });
