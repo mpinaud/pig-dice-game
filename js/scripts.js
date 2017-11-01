45138ee5114477a093b4ae1c1572f78ec5f1d438
Chris Johnson and Mikey Pinaud <mikey@>
2017-11-01 11:10:48 -0700

    add playgame function to end game at score 100 and endturn function to switch players

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,51 @@
-function roll(){
+function rollDice(){
   var randomRoll = Math.floor(Math.random()) + 1; // (0 - 0.9999999) * 6
   return randomRoll;
 }
 
-function Player(playerName) = {
+function roll(){
+
+}
+
+function Player(playerName){
   this.name = playerName;
   this.score = 0;
 }
 
+function endTurn(players){
+  if (endTurn = 0){
+    endTurn = 1;
+  } else {
+    endTurn = 0;
+  }
+}
+
+function playGame(players){
+  var keepPlaying = true;
+  while (keepPlaying){
+    players.forEach(function(player){
+      if (player.score >= 100) {
+        keepPlaying = false;
+        alert(player.name + ' Wins!');
+      }
+    });
+  }
+}
 
 
 $(function(){
+  var players;
   $("button#name-button").click(function(){
+    players = [];
     var player1Name = $("#player-1-name-input").val();
     var player2Name = $("#player-2-name-input").val();
-    alert(player2Name);
+    players.push(new Player(player1Name));
+    players.push(new Player(player2Name));
+    console.log(players);
+  });
+  $("button#start-game-button").click(function(){
+    // $("").hide() // hide upper area
+    playGame(players);
   });
 
   $("")
